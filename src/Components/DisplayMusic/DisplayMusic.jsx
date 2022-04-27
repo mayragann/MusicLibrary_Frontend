63ca7cc44270f15752e5499b97b2dc5d5fa967e1
mayragann <mayragann@gmail.com>
2022-04-27 13:28:46 -0400

    Added DisplayMusic Component Table

---
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,31 @@
+const DisplayMusic = (props) => {
+    console.log(props.songs)
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                    <th>Likes</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.parentSongs.map((song) => (
+                        <tr key={song.id}>
+                            <td>{song.title}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.album}</td>
+                            <td>{song.release_date}</td>
+                            <td>{song.genre}</td>
+                            <td>{song.likes}</td>
+                            </tr>
+                    ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default DisplayMusic;
